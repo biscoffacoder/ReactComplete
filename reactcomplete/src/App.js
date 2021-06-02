1f3021997fa9296839f68fe40d2ee3e5ddb545f2
Allan <biscoffacoder@yahoo.com>
2021-06-02 13:13:14 +0530

    Functional Components commit

--- reactcomplete/src/App.js
+++ reactcomplete/src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 import Counter from './functionalComponents/Counter';
+import React, { useState } from 'react';
+import MainHeader from './components1/MainHeader/MainHeader';
+import Login from './components1/Login/Login';
+import Home from './components1/Home/Home';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div className="App">
-      {/* <ExpenseItem /> */}
-      <Counter/>
-    </div>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
 export default App;
+
